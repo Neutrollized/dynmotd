6252de14707691b21653ad0d754c1a12307d4f3c
Neutrollized <glen.yu@gmail.com>
2018-07-30 19:25:36 -0400

    added a space in the grep for the filesystem mountpoint

--- example/dynmotd.d/filesystem_check.sh
+++ example/dynmotd.d/filesystem_check.sh
@@ -1,7 +1,7 @@
 #file systems
-ROOT_LV=$(df -Ph | grep "/$" | awk '{print $4,"-",$5}' | tr -d '\n')
-OPT_LV=$(df -Ph | grep "/opt$" | awk '{print $4,"-",$5}' | tr -d '\n')
-VARLOG_LV=$(df -Ph | grep "/var/log$" | awk '{print $4,"-",$5}' | tr -d '\n')
+ROOT_LV=$(df -Ph | grep " /$" | awk '{print $4,"-",$5}' | tr -d '\n')
+OPT_LV=$(df -Ph | grep " /opt$" | awk '{print $4,"-",$5}' | tr -d '\n')
+VARLOG_LV=$(df -Ph | grep " /var/log$" | awk '{print $4,"-",$5}' | tr -d '\n')
 
 echo -e "===== FILESYSTEM ==========================================================
  $COLOR_COLUMN- /$RESET_COLORS..................: $COLOR_VALUE ${ROOT_LV} (avail-use%) $RESET_COLORS
