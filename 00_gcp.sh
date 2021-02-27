1a2d2233953f80a885180729f74b13ababafe404
Neutrollized <glen.yu@gmail.com>
2021-02-27 18:26:01 -0500

    added extra metadata for gcp

--- 00_gcp.sh
+++ 00_gcp.sh
@@ -1,7 +1,12 @@
 # https://cloud.google.com/appengine/docs/standard/java/accessing-instance-metadata
-GCP_PROJECT=$(curl -s "metadata.google.internal/computeMetadata/v1/project/project-id" -H "Metadata-Flavor: Google")
-GCP_ZONE=$(curl -s "metadata.google.internal/computeMetadata/v1/instance/zone" -H "Metadata-Flavor: Google" | xargs basename)
+# https://cloud.google.com/compute/docs/reference/rest/v1/instances/list
+GCP_PROJECT=$(curl -s -H "Metadata-Flavor: Google" "metadata.google.internal/computeMetadata/v1/project/project-id")
+GCP_INSTANCE_MACHINE_TYPE=$(curl -s -H "Metadata-Flavor: Google" "metadata.google.internal/computeMetadata/v1/instance/machine-type" | xargs basename)
+GCP_INSTANCE_PREEMPTIBLE=$(curl -s -H "Metadata-Flavor: Google" "metadata.google.internal/computeMetadata/v1/instance/scheduling/preemptible")
+GCP_INSTANCE_ZONE=$(curl -s -H "Metadata-Flavor: Google" "metadata.google.internal/computeMetadata/v1/instance/zone" | xargs basename)
 
 echo -e "===== GCP INSTANCE METADATA ==================================================
  ${COLOR_COLUMN}- Project ID${RESET_COLORS}.........: ${COLOR_VALUE} ${GCP_PROJECT} ${RESET_COLORS}
- ${COLOR_COLUMN}- Zone${RESET_COLORS}...............: ${COLOR_VALUE} ${GCP_ZONE} ${RESET_COLORS}"
+ ${COLOR_COLUMN}- Machine Type${RESET_COLORS}.......: ${COLOR_VALUE} ${GCP_INSTANCE_MACHINE_TYPE} ${RESET_COLORS}
+ ${COLOR_COLUMN}- Preemptible${RESET_COLORS}........: ${COLOR_VALUE} ${GCP_INSTANCE_PREEMPTIBLE} ${RESET_COLORS}
+ ${COLOR_COLUMN}- Zone${RESET_COLORS}...............: ${COLOR_VALUE} ${GCP_INSTANCE_ZONE} ${RESET_COLORS}"
