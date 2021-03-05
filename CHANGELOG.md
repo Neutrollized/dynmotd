abc5f37497c5309e4e18ba0342c6fde463b083ce
Neutrollized <glen.yu@gmail.com>
2021-03-05 11:41:00 -0500

    v2.0.4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## [2.0.4] - 2021-03-05
+### Added
+- added verbostiy to the install script
+### Fixed
+- added `--max-time 1` to the curl command when checking the internal metadata IP address as some would not give a valid response right away and would have to wait for a long timeout
+
 ## [2.0.3] - 2021-03-03
 ### Added
 - detects Oracle Linux as a distro
--- install.sh
+++ install.sh
@@ -1,21 +1,26 @@
 #! /bin/bash
 set +x
 
+echo '+ starting install'
 DYNMOTD_BIN_PATH='/usr/local/bin'
 DYNMOTD_CUSTOM_SCRIPTS_PATH='/etc/dynmotd.d'
 
 
+echo ' + installing dynmotd'
 cp ./dynmotd ${DYNMOTD_BIN_PATH}/dynmotd
 chmod 755 ${DYNMOTD_BIN_PATH}/dynmotd
 ln -s ${DYNMOTD_BIN_PATH}/dynmotd ${DYNMOTD_BIN_PATH}/dm 2>/dev/null
 
+echo '  + setting up MOTD'
 echo "${DYNMOTD_BIN_PATH}/dynmotd" > /etc/profile.d/dynmotd.sh
 chmod 644 /etc/profile.d/dynmotd.sh
 
+echo '  + creating custom scripts folder'
 mkdir -p ${DYNMOTD_CUSTOM_SCRIPTS_PATH}
 rm ${DYNMOTD_CUSTOM_SCRIPTS_PATH}/00_*.sh 2>/dev/null
 
 
+echo ' + detecting additional OS info'
 # check to see if it's a Raspberry Pi
 cat /etc/os-release | grep -q 'Raspbian'
 if [ $? -eq 0 ]
@@ -24,22 +29,23 @@ then
   cp ./00_raspberry_pi.sh ${DYNMOTD_CUSTOM_SCRIPTS_PATH}/.
 fi
 
-
+echo '  + checking for cloud provider status'
 # check to see if VM belongs to one of the big 3 CSPs
 # done via checking against https://ipinfo.io/, getting the org and cutting out the asn
 # then checking to see if the metadata server/internal address responds
 IPINFO_ORG=$(curl -s ipinfo.io/org | awk {'first = $1; $1=""; print $0'}|sed 's/^ //g')
 
-if [[ 'Google LLC' = "${IPINFO_ORG}" && $(curl --write-out %{http_code} -s --output /dev/null 169.254.169.254) == '200' ]]
+if [[ 'Google LLC' = "${IPINFO_ORG}" && $(curl --max-time 1 --write-out %{http_code} -s --output /dev/null 169.254.169.254) == '200' ]]
 then
   cp ./00_gcp.sh ${DYNMOTD_CUSTOM_SCRIPTS_PATH}/.
-elif [[ 'Amazon.com, Inc.' = "${IPINFO_ORG}" && $(curl --write-out %{http_code} -s --output /dev/null 169.254.169.254) == '200' ]]
+elif [[ 'Amazon.com, Inc.' = "${IPINFO_ORG}" && $(curl --max-time 1 --write-out %{http_code} -s --output /dev/null 169.254.169.254) == '200' ]]
 then
   cp ./00_aws.sh ${DYNMOTD_CUSTOM_SCRIPTS_PATH}/.
-elif [[ 'Microsoft Corporation' = "${IPINFO_ORG}" && $(curl --write-out %{http_code} -s --output /dev/null 169.254.169.254) == '400' ]]
+elif [[ 'Microsoft Corporation' = "${IPINFO_ORG}" && $(curl --max-time 1 --write-out %{http_code} -s --output /dev/null 169.254.169.254) == '400' ]]
 then
   cp ./00_azure.sh ${DYNMOTD_CUSTOM_SCRIPTS_PATH}/.
 fi
 
 
+echo '+ install complete!'
 ${DYNMOTD_BIN_PATH}/dynmotd
