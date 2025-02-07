7e01ab2adc718b7ba47402e3e52a8b077de6c728
Glen Yu <glen.yu@gmail.com>
2025-02-06 23:13:54 -0500

    v3.4.0, added raspberry pi bootloader and gpu mem info
    Signed-off-by: Glen Yu <glen.yu@gmail.com>

--- 00_raspberry_pi.sh
+++ 00_raspberry_pi.sh
@@ -1,8 +1,12 @@
 # the "tr" command is to suppress the "warning: command substitution: ignored null byte in input" errors
 # https://askubuntu.com/questions/926626/how-do-i-fix-warning-command-substitution-ignored-null-byte-in-input
 PI_REVISION=$(cat /proc/device-tree/model | tr '\0' '\n')
+PI_BOOTLOADER=$(vcgencmd bootloader_version | awk 'NR==1 {printf "%s ", $0} NR==3 {printf "(%s)", $2}')
+PI_GPU_MEM=$(vcgencmd get_mem gpu | awk -F'=' '{ print $2 }')
 PI_TEMP=$(vcgencmd measure_temp | awk -F'=' '{ print $2 }')
 
 echo -e "===== PI INFO ================================================================
  ${COLOR_COLUMN}- Revision${RESET_COLORS}...........: ${COLOR_VALUE} ${PI_REVISION} ${RESET_COLORS}
+ ${COLOR_COLUMN}- Bootloader${RESET_COLORS}.........: ${COLOR_VALUE} ${PI_BOOTLOADER} ${RESET_COLORS}
+ ${COLOR_COLUMN}- GPU memory${RESET_COLORS}.........: ${COLOR_VALUE} ${PI_GPU_MEM} ${RESET_COLORS}
  ${COLOR_COLUMN}- Temperature${RESET_COLORS}........: ${COLOR_VALUE} ${PI_TEMP} ${RESET_COLORS}"
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## [3.4.0] - 2025-02-06
+### Added
+- Raspberry Pi bootloader version info
+- Raspberry Pi GPU memory allocation
+
 ## [3.3.0] - 2024-09-20
 ### Fixed
 - added extra condition when detecting when OS is a WSL distro to remove some false positive results
