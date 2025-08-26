f73cfa393082748c22b0020d06ed72a20488092b
Glen Yu <glen.yu@gmail.com>
2025-08-25 21:53:57 -0400

    updated example services check
    Signed-off-by: Glen Yu <glen.yu@gmail.com>

--- example/dynmotd.d/20_services_check.sh
+++ example/dynmotd.d/20_services_check.sh
@@ -2,9 +2,8 @@
 # the output of the command below would be something like:
 # active (running) since Fri 2018-08-03 09:37:39 CDT
 # where as the others would just simply say 'active'
-# DOCKER_SVC=$(systemctl status docker | grep 'Active:' | awk -F': ' '{$1=""; print $0}' | awk -F';' '{print $1}' | xargs)
+DOCKER_SVC=$(systemctl status docker | grep 'Active:' | awk -F': ' '{$1=""; print $0}' | awk -F';' '{print $1}' | xargs)
 
-DOCKER_SVC=$(systemctl status docker | grep 'Active:' | awk '{ print $2 }')
 JENKINS_SVC=$(systemctl status jenkins | grep 'Active:' | awk '{ print $2 }')
 RUNDECKD_SVC=$(systemctl status rundeckd | grep 'Active:' | awk '{ print $2 }')
 SUPERVISORD_SVC=$(systemctl status supervisord | grep 'Active:' | awk '{ print $2 }')
