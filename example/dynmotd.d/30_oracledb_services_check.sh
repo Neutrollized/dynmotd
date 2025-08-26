14b28ee7e74b9047371a1aa12b68d07adeb6ad7c
Glen Yu <glen.yu@gmail.com>
2025-08-25 21:51:45 -0400

    fixed width in example scripts
    Signed-off-by: Glen Yu <glen.yu@gmail.com>

--- example/dynmotd.d/30_oracledb_services_check.sh
+++ example/dynmotd.d/30_oracledb_services_check.sh
@@ -2,6 +2,6 @@
 ORACLE_SVC_LISTENER=$(sudo service oracle-xe-21c status | grep 'LISTENER status:' | awk '{ for (i=3; i<=NF; i++) printf "%s%s", $i, (i==NF ? "" : OFS); print "" }')
 ORACLE_SVC_DB=$(sudo service oracle-xe-21c status | grep 'Database status:' | awk '{ for (i=4; i<=NF; i++) printf "%s%s", $i, (i==NF ? "" : OFS); print "" }')
 
-echo -e "===== ORACLE DB STATUS =======================================================
+echo -e "===== ORACLE DB STATUS ========================================================
  $COLOR_COLUMN- Listener$RESET_COLORS...........: $COLOR_VALUE ${ORACLE_SVC_LISTENER}$RESET_COLORS
  $COLOR_COLUMN- Database$RESET_COLORS...........: $COLOR_VALUE ${ORACLE_SVC_DB}$RESET_COLORS"
